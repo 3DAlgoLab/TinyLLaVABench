e646dd2d75f8e6ca467a4e7ea241cb19f26e84e7
3D ALGO LAB <threealgo@outlook.com>
2024-04-23 11:11:23 +0900

    finetune lora

--- experiments/finetune_lora.sh
+++ experiments/finetune_lora.sh
@@ -2,16 +2,16 @@
 
 # Assign the arguments to variables
 DATA_PATH="/dev_repo/VLM/TinyLLaVABench/experiments/pokemon/pokemon_blip_captions.json"
-IMAGE_PATH="/dev_repo/VLM/TinyLLaVABench/experiments/pokemon/image"
+IMAGE_PATH="/dev_repo/VLM/TinyLLaVABench/experiments/"
 OUTPUT_DIR="/dev_repo/VLM/TinyLLaVABench/experiments/TinyLLaVA-3.1B-lora"
 
-deepspeed tinyllava/train/train.py \
+deepspeed ../tinyllava/train/train.py \
     --deepspeed ../scripts/tiny_llava/zero3.json \
     --lora_enable True --lora_r 32 --lora_alpha 64 \
-    --model_name_or_path /root/autodl-tmp/TinyLLaVA-3.1B \
+    --model_name_or_path bczhou/TinyLLaVA-3.1B \
     --version phi \
     --data_path $DATA_PATH \
-    --image_folder $IMAGE_PATH --vision_tower /root/autodl-tmp/TinyLLaVA-3.1B-SigLIP \
+    --image_folder $IMAGE_PATH --vision_tower bczhou/TinyLLaVA-3.1B-SigLIP \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -20,10 +20,10 @@ deepspeed tinyllava/train/train.py \
     --group_by_modality_length False \
     --fp16 True \
     --output_dir $OUTPUT_DIR \
-    --num_train_epochs 3 \
-    --per_device_train_batch_size 8 \
+    --num_train_epochs 6 \
+    --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 2 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
